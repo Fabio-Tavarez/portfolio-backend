f691fed667ad4300633930e4ca4753bba1d48dbb
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-08-20 21:12:54 -0400

    books controller done

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,3 +1,62 @@
 const express = require("express");
+const router = express.Router();
 
-const { getAllBooks, getBooksById } = require("../queries/book");
+const {
+  getAllBooks,
+  getBooksById,
+  createBook,
+  deleteBook,
+  updateBookById,
+} = require("../queries/book");
+
+router.get("/", async (req, res) => {
+  const allBooks = await getAllBooks();
+
+  if (Array.isArray(allBooks)) {
+    res.json(allBooks);
+  } else {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const book = await getBooksById(id);
+
+  if (book.length === 0) {
+    res.status(404).json({ error: "not found" });
+  } else {
+    res.json(book[0]);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const createdBook = await createBook(req.body);
+
+  res.json(createdBook);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedBook = await deleteBook(id);
+
+  if (deletedBook.length === 0) {
+    return res.status(404).json({ message: "No data found!", error: true });
+  } else {
+    return res.json(deletedBook[0]);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedBook = await updateBookById(id, req.body);
+  console.log(updatedBook);
+  if (updatedBook.length === 0) {
+    res.status(404).json({ message: "not found!", error: true });
+  } else {
+    res.json(updatedBook[0]);
+  }
+});
+
+module.exports = router;
